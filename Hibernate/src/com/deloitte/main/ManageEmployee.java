2f84a3b794124892602c6e09549ad8918026f3f2
M8 <M8@192.168.18.31>
2018-08-06 17:12:10 +0530

    IC

--- Hibernate/src/com/deloitte/main/ManageEmployee.java
+++ Hibernate/src/com/deloitte/main/ManageEmployee.java
@@ -3,6 +3,7 @@ package com.deloitte.main;
 import java.util.Iterator;
 import java.util.List;
 
+import org.hibernate.Criteria;
 import org.hibernate.HibernateException;
 import org.hibernate.SQLQuery;
 import org.hibernate.Session;
@@ -10,6 +11,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 //import org.hibernate.mapping.List;
+import org.hibernate.criterion.Projections;
 
 import com.deloitte.pojo.Employee;
 
@@ -30,14 +32,12 @@ public class ManageEmployee {
   Integer empID1 = ME.addEmployee("Zara", "Ali", 1000);
   Integer empID2 = ME.addEmployee("Daisy", "Das", 5000);
   Integer empID3 = ME.addEmployee("John", "Paul", 10000);
-  ME.updateEmployee(empID1, 5000);
-  /* Delete an employee from the database */
-  ME.deleteEmployee(empID2);
-
-  /* List down new list of the employees */
-  ME.listEmployees();
+  ME.updateEmployee(empID1, 52000);
+  
   /* List down all the employees */
   ME.listEmployees();
+  ME.countEmployee();
+  ME.totalSalary();
 
  }
 
@@ -144,5 +144,74 @@ public class ManageEmployee {
        session.close(); 
     }
  }
- 		
+ public void listEmployeesEntityNS2( ){
+     Session session = factory.openSession();
+     Transaction tx = null;
+     
+     try {
+        tx = session.beginTransaction();
+        String sql = "FROM EMPLOYEE E ORDER BY E.SALARY";
+        SQLQuery query = session.createSQLQuery(sql);
+        query.addEntity(Employee.class);
+        List employees = query.list();
+
+        for (Iterator iterator = employees.iterator(); iterator.hasNext();){
+           Employee employee = (Employee) iterator.next(); 
+           System.out.print("First Name: " + employee.getFirstName()); 
+           System.out.print("  Last Name: " + employee.getLastName()); 
+           System.out.println("  Salary: " + employee.getSalary()); 
+        }
+        tx.commit();
+     } catch (HibernateException e) {
+        if (tx!=null) tx.rollback();
+        e.printStackTrace(); 
+     } finally {
+        session.close(); 
+     }
+  }
+ 
+ public void countEmployee(){
+     Session session = factory.openSession();
+     Transaction tx = null;
+     
+     try {
+        tx = session.beginTransaction();
+        Criteria cr = session.createCriteria(Employee.class);
+
+        // To get total row count.
+        cr.setProjection(Projections.rowCount());
+        List rowCount = cr.list();
+
+        System.out.println("Total Coint: " + rowCount.get(0) );
+        tx.commit();
+     } catch (HibernateException e) {
+        if (tx!=null) tx.rollback();
+        e.printStackTrace(); 
+     } finally {
+        session.close(); 
+     }
+  }
+ 
+  /* Method to print sum of salaries */
+  public void totalSalary(){
+     Session session = factory.openSession();
+     Transaction tx = null;
+     
+     try {
+        tx = session.beginTransaction();
+        Criteria cr = session.createCriteria(Employee.class);
+
+        // To get total salary.
+        cr.setProjection(Projections.sum("salary"));
+        List totalSalary = cr.list();
+
+        System.out.println("Total Salary: " + totalSalary.get(0) );
+        tx.commit();
+     } catch (HibernateException e) {
+        if (tx!=null) tx.rollback();
+        e.printStackTrace(); 
+     } finally {
+        session.close(); 
+     }
+  }
 } 
